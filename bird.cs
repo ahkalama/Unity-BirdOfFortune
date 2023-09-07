1994b60eacc3e3cdd2ed5daff24781803297be47
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-09-08 00:02:08 +0300

    kusazıplamaeklendi

--- bird.cs
+++ bird.cs
@@ -6,11 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class bird : MonoBehaviour
 {
-   void OnCollisionEnter2D(Collision2D collision)
+    public float jumpForce = 10.0f; // Kuşun zıplama kuvveti
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "lav")
         {
-             SceneManager.LoadScene(2);
+            SceneManager.LoadScene(2);
+        }
+
+        if (collision.gameObject.tag == "zizip")
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero; // Mevcut hızı sıfırla
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // Zıplama kuvveti uygula
         }
     }
 }
